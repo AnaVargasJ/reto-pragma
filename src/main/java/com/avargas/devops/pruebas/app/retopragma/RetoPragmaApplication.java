28a063836dbf8236c37c2c63a594081d6719464a
AnaVargasJ <anamariavargasj1590@gmail.com>
2025-05-07 23:38:01 -0500

    1er commit

---
+++ src/main/java/com/avargas/devops/pruebas/app/retopragma/RetoPragmaApplication.java
@@ -0,0 +1,13 @@
+package com.avargas.devops.pruebas.app.retopragma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoPragmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoPragmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avargas/devops/pruebas/app/retopragma/RetoPragmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avargas.devops.pruebas.app.retopragma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoPragmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
